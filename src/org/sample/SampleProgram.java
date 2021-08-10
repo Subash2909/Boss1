035edb839e11b539e1eab8ae07d65c277b309cd5
Subash2909 <rajasubash2909@gmail.com>
2021-08-10 23:43:14 +0530

    my 1st push

---
+++ src/org/sample/SampleProgram.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class SampleProgram {
+static{
+	System.out.println("Static Block");
+}
+public SampleProgram() {
+
+System.out.println("This is Constructor");
+
+}
+public void abc() {
+	System.out.println("this is instance method");// TODO Auto-generated method stub
+
+}
+	public static void main(String[] args) {
+	 	SampleProgram program=new SampleProgram();
+	program.abc();
+		
+	}
+
+}
