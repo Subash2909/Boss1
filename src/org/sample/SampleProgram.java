33639a065032f635cfd19bf7a5eaf5be621d0e9c
Subash2909 <rajasubash2909@gmail.com>
2021-08-11 00:01:57 +0530

    boss 1st push

--- src/org/sample/SampleProgram.java
+++ src/org/sample/SampleProgram.java
@@ -12,6 +12,9 @@ System.out.println("This is Constructor");
 public void abc() {
 	System.out.println("this is instance method");// TODO Auto-generated method stub
 
+}public  void cba() {
+System.out.println(" project sample");
+
 }
 	public static void main(String[] args) {
 	 	SampleProgram program=new SampleProgram();
